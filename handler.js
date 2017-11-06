92d523a6c153e579ab9edd1e9bc9fad40004520b
Kris Keener <wt5v@outlook.com>
2017-11-06 13:57:57 +0000

    First Commit

---
+++ handler.js
@@ -0,0 +1,17 @@
+'use strict';
+
+module.exports.hello = (event, context, callback) => {
+  let remainingTime = context.getRemainingTimeInMillis();
+  let functionName = context.functionName;
+  let AWSrequestID = context.awsRequestId;
+  
+  const response = {
+    statusCode: 200,
+    ev: event,
+    rt: remainingTime,
+    fn: functionName,
+    aid: AWSrequestID
+  };
+  
+  callback(null, response);
+};
